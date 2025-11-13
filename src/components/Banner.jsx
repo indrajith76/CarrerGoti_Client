40c963609a5817d2f7ca75b8f8456109570d2f93
indrajith76 <indrajithgoswami@gmail.com>
2025-11-14 01:01:45 +0600

    [Banner]:update banner iimages

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="carousel w-full h-[80vh]">
-      <div id="slide1" className="carousel-item relative w-full">
+      <div id="slide1" className="carousel-item relative w-full overflow-hidden">
         <img
-          src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-          className="w-full"
+          src="https://images.ctfassets.net/gogvzi849aaj/1KPO3rlI6jGaq1jFeWMGZr/c33f946ea0f8367188528a4f9d0f239c/CareerService_Hero.jpeg"
+          className="w-full object-cover"
         />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a href="#slide4" className="btn btn-circle">
@@ -19,8 +19,8 @@ const Banner = () => {
       </div>
       <div id="slide2" className="carousel-item relative w-full">
         <img
-          src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-          className="w-full"
+          src="https://images.ctfassets.net/gogvzi849aaj/9Nn3FEMCdO5ac6oCsnots/7370e7746ddd16f883f3639a0c376f55/students.png"
+          className="w-full object-cover"
         />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a href="#slide1" className="btn btn-circle">
@@ -33,8 +33,8 @@ const Banner = () => {
       </div>
       <div id="slide3" className="carousel-item relative w-full">
         <img
-          src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-          className="w-full"
+          src="https://media.istockphoto.com/id/1162354887/photo/businesspeople-handshaking-after-deal-or-interview.jpg?s=612x612&w=0&k=20&c=G4dic7x9xgmnDLB5HwZ-R9ZQOfbQ_hqyIyaHtl23K7M="
+          className="w-full object-cover"
         />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a href="#slide2" className="btn btn-circle">
--- src/pages/Dashboard/ManageAllUsers.jsx
+++ src/pages/Dashboard/ManageAllUsers.jsx
@@ -1,11 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "../../api/axios";
+import { FaBan } from "react-icons/fa";
 
 const ManageAllUsers = () => {
-    return (
-        <div>
-            
+  const [users, setUsers] = useState([]);
+  const [UsersLoader, setUsersLoader] = useState(true);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setUsersLoader(true);
+      try {
+        const res = await axios.get("/api/auth/users");
+        if (res.data.success) {
+          setUsers(res.data.data);
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setUsersLoader(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className="mb-10">
+      <h2 className="text-2xl">Manage Job Posts</h2>
+      {UsersLoader ? (
+        <div className="flex items-center justify-center mt-10">
+          <span className="loading loading-spinner loading-xl"></span>
         </div>
-    );
+      ) : (
+        <div className="overflow-x-auto mt-10">
+          <table className="table table-xs">
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>image</th>
+                <th>name</th>
+                <th>email</th>
+                <th>phone</th>
+                <th>role</th>
+                <th>createdAt</th>
+                <th className="text-center">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((u, idx) => (
+                <tr>
+                  <th>{idx + 1}</th>
+                  <td>
+                    <img
+                      className="w-10 rounded-full border border-gray-400"
+                      src="https://t4.ftcdn.net/jpg/02/44/43/69/360_F_244436923_vkMe10KKKiw5bjhZeRDT05moxWcPpdmb.jpg"
+                      alt=""
+                    />
+                    {/* {u?.image} */}
+                  </td>
+                  <td>{u?.name}</td>
+                  <td>{u?.email}</td>
+                  <td>{u?.phone}</td>
+                  <td>{u?.role}</td>
+                  <td>{u?.createdAt}</td>
+                  <td className="mx-auto flex items-center">
+                    <button className="btn btn-sm btn-error text-white mx-auto">
+                      <FaBan /> Ban User
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ManageAllUsers;
